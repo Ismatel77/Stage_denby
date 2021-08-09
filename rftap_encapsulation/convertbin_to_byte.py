06944510915b8e9f37059850174f68bb879b85f1
AlexandraHULOT <59099389+AlexandraHULOT@users.noreply.github.com>
2021-08-09 19:13:09 +0200

    change script to handle config file

--- rftap_encapsulation/convertbin_to_byte.py
+++ rftap_encapsulation/convertbin_to_byte.py
@@ -1,31 +1,57 @@
 import getopt
 import sys
 
-
 inputFile = ''
 outputFile = ''
+configFile = ''
 
 ##### GET INPUT/OUTPUT FILES FROM COMMAND LINE #####
 argv = sys.argv[1:]
-opts,args = getopt.getopt(argv,'f:o:')
+opts,args = getopt.getopt(argv,'f:o:c:')
 for (opt,value) in opts:
     if opt == '-h':
-        print("Command options :\n-f  input file\n-o  output file")
+        print("Command options :\n-f  input file\n-o  output file\-c    config file")
         exit()
     elif opt == '-f':
         inputFile = value
     elif opt == '-o':
         outputFile = value
+    elif opt == '-c':
+        configFile = value
     else :
         print("Failed.Option "+opt+" is not recognized.")
         exit()
-if(inputFile == '' or outputFile  == ''):
+if(inputFile == '' or outputFile  == '' or configFile == ''):
     print("Conversion failed. You must specify the paths of all the required files in the command line.")
     exit()
 
-#### READ AND WRITE ####
+### READ AND WRITE CONFIGFILE"
+f_output = open("rftap_param.config", "w")
+
+def without_outliers(list, max):
+    list.sort()
+    median = list[len(list)//2]
+    return [x for x in list if abs(x - median) < max]
+
+def mean(list):
+    return sum(list)/len(list)
+
+rssi = []
+snr = []
+for line in open(configFile, "r"):
+    if line.startswith("snr"):
+        snr.append(int(line.split("=")[1].strip()))    
+    elif line.startswith("rssi"):
+        rssi.append(int(line.split("=")[1].strip()))
+
+snr = without_outliers(snr, 1000)
+rssi = without_outliers(rssi, 1000)
+
+f_output.write("[headers]\nsnr = {}\nrssi = {}\n".format(mean(snr), mean(rssi)))
+f_output.close()
+
+#### READ INPUTFILE AND WRITE IN OUTPUTFILE####
 byteFile = open(outputFile, 'wb')
-print("\nConverting binary data into bytes...")
 binFile = filter(str.isdigit, open(inputFile, "r").read())
 byteFile.write(bytearray((int(binFile[i:i+8], 2) for i in range(0, len(binFile), 8))))
 byteFile.close()
--- rftap_encapsulation/rftap_encapsulation.sh
+++ rftap_encapsulation/rftap_encapsulation.sh
@@ -2,6 +2,7 @@
 
 input_file=res.txt
 output_file=out.txt
+configFile=raw_headers.txt
 nb_events=0
 chmod a+w .
 inotifywait -e close_write -m . |
@@ -10,8 +11,9 @@ while read -r directory events filename ; do
     nb_events=$(($nb_events + 1))
     if [ $nb_events = 2 ];then
       sleep 0.01
-      python2 convertbin_to_byte.py -f $input_file -o $output_file &
+      python2 convertbin_to_byte.py -f $input_file -o $output_file -c $configFile &
       python2 gnuradio_rftap_encapsulation.py &
+      echo "A new frame has been sent."
       sleep 0.005
       kill -INT $!
       nb_events=0
