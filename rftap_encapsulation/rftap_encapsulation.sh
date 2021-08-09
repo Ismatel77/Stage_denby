afade7d30170001b68e55bc84020846f808c563e
AlexandraHULOT <59099389+AlexandraHULOT@users.noreply.github.com>
2021-08-09 15:57:53 +0200

    v2

--- rftap_encapsulation/rftap_encapsulation.sh
+++ rftap_encapsulation/rftap_encapsulation.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-inotifywait -e close_write,create -m . |
-while read -r directory events filename; do
-  if [ "$filename" = "res.txt" ]; then
-    python2 convertbin_to_byte.py -f ./res.txt -o ./out.txt 
-    python2 gnuradio_rftap_encapsulation.py
+input_file=res.txt
+output_file=out.txt
+nb_events=0
+chmod a+w .
+inotifywait -e close_write -m . |
+while read -r directory events filename ; do
+  if [ "$filename" = "$input_file" ];then
+    nb_events=$(($nb_events + 1))
+    if [ $nb_events = 2 ];then
+      sleep 0.01
+      python2 convertbin_to_byte.py -f $input_file -o $output_file &
+      python2 gnuradio_rftap_encapsulation.py &
+      sleep 0.005
+      kill -INT $!
+      nb_events=0
+    fi
   fi
 done
 
